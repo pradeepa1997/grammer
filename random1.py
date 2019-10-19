7d8a60ba91f78cc0086068896ffd5faec98e929a
pradeepa sandaruwan <pradeepasandharuwan@gmail.com>
2019-10-19 06:55:33 +0530

    first commit

---
+++ random1.py
@@ -0,0 +1,53 @@
+import math
+
+
+class random:
+
+    def __init__(self, seed):
+
+        self.m_min= seed
+        self.m_max= 2147483648
+        self.no =seed
+
+        if(seed<1 or seed>self.m_max-2):
+              print("out of range")
+
+
+    def  next(self):
+        a = self.m_min
+        m= self.m_max
+        self.no = (16807*self.no)%(2147483648-1)
+        print(self.no)
+        return self.no
+
+    def choose(self, limit):
+        new_no=self.next()
+        new_no = new_no%limit
+        print(new_no)
+        return new_no
+
+
+a=random(10)
+print(a.next())
+a.choose(10)
+
+print(a.next())
+a.choose(10)
+
+print(a.next())
+a.choose(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
